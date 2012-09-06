e0eca2b96277d9051ed5b2871e488a490accf3f7
Gaurav Agarwal <gauravag@thoughtworks.com>
2012-09-06 19:27:34 +0530

    Turned node.js to Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rideshare::Application.config.secret_token = 'a6d97f205a4a7d5eea6850d32142adf06d546243aa4b5206ce552dfa9d19897aceea70aed7d06778682df755721509494ac3e2211071b82b2b6892f553082fd2'
